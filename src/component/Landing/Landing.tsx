7120c52b9e4b1ac736822549a5e476454e0935e3
mahdihaghollahi <mahdiaghollahi@gmail.com>
2024-11-09 18:26:19 +0330

    add landing page

--- src/component/Landing/Landing.tsx
+++ src/component/Landing/Landing.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import LandingLogo from '@/assent/Img/Landing/LandingLogo.svg'
-import Image from 'next/image'
+import React, { Fragment } from "react";
+import LandingLogo from "@/assent/Img/Landing/LandingLogo.svg";
+import Image from "next/image";
 const Landing = () => {
   return (
-    <>
-    <div className='flex  flex-col py-20 justify-center items-center'>
-      <Image
-      src={LandingLogo}
-      width={315}
-      height={273}
-      alt=''
-     className=''
-      />
-      <h1 className='font-bold mt-20 text-3xl text-[#000000] leading-10'>
-      Explore the app
-      </h1>
-    </div>
-    </>
-  )
-}
+    <Fragment>
+      <div className="flex  flex-col mt-20 justify-center items-center">
+        <Image src={LandingLogo} width={315} height={273} alt="" className="" />
+        <h1 className="font-bold mt-20 text-3xl text-[#000000] leading-10">
+          Explore the app
+        </h1>
+        <p className="font-light text-lg leading-8 text-[#000000B2] whitespace-normal text-center xl:leading-5 xl:whitespace-nowrap mt-10">
+          Now your finances are in one place and always under control
+        </p>
+        <button className="mt-20 text-white font-semibold text-base leading-5 text-center bg-[#000000] py-4 px-36 xl:px-[13%] rounded-xl ">
+          Log in
+        </button>
 
-export default Landing
+        <button className="mt-5 text-black font-semibold text-base leading-5 text-center bg-[#ffff] border border-[#747474] py-4 px-[24.5%] xl:px-[10%] rounded-xl ">
+          Create account
+        </button>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Landing;
